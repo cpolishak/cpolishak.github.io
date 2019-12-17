becb4638d875f5714111b305d8b1c2d45cb233f2
Chris Polishak <chris@spectre.lan1>
2019-12-16 21:43:00 -0600

    Added some more js projects to dev

--- app.js
+++ app.js
@@ -1,69 +1,69 @@
-// For rotating image display of photos //
+// // For rotating image display of photos //
 
-var images = [
-  "/media/flowers1.jpg", "/media/glass.jpg", "/media/vizcarra.jpg", "/media/macanitas.jpg"
-];
+// var images = [
+//   "/media/flowers1.jpg", "/media/glass.jpg", "/media/vizcarra.jpg", "/media/macanitas.jpg"
+// ];
 
-var i = 0;
-var img = document.getElementById('macanitas');
+// var i = 0;
+// var img = document.getElementById('macanitas');
 
-function changeImg() {
-  img.src = images[i];
+// function changeImg() {
+//   img.src = images[i];
 
-  if(i < images.length - 1) {
-      i++;
-  } else {
-      i = 0;
-  }
-  console.log(i);
-}
+//   if(i < images.length - 1) {
+//       i++;
+//   } else {
+//       i = 0;
+//   }
+//   console.log(i);
+// }
 
-setInterval(changeImg, 5000);
+// setInterval(changeImg, 5000);
 
-function stopChg() {
-  // make this do a pause on an image when clicked. 
-  // when released it will allow the images to keep changing at each interval.
-}
+// function stopChg() {
+//   // make this do a pause on an image when clicked. 
+//   // when released it will allow the images to keep changing at each interval.
+// }
 
 
 
-// For gallery/modal/lightbox on gallery page //
-function openModal() {
-    document.getElementById("myModal").style.display = "block";
-  }
+// // For gallery/modal/lightbox on gallery page //
+// function openModal() {
+//     document.getElementById("myModal").style.display = "block";
+//   }
   
-  function closeModal() {
-    document.getElementById("myModal").style.display = "none";
-  }
+//   function closeModal() {
+//     document.getElementById("myModal").style.display = "none";
+//   }
   
-  var slideIndex = 1;
-  showSlides(slideIndex);
+//   var slideIndex = 1;
+//   showSlides(slideIndex);
   
-  function plusSlides(n) {
-    showSlides(slideIndex += n);
-  }
+//   function plusSlides(n) {
+//     showSlides(slideIndex += n);
+//   }
   
-  function currentSlide(n) {
-    showSlides(slideIndex = n);
-  }
+//   function currentSlide(n) {
+//     showSlides(slideIndex = n);
+//   }
   
-  function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("demo");
-    var captionText = document.getElementById("caption");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
-    captionText.innerHTML = dots[slideIndex-1].alt;
-  }
+//   function showSlides(n) {
+//     var i;
+//     var slides = document.getElementsByClassName("mySlides");
+//     var dots = document.getElementsByClassName("demo");
+//     var captionText = document.getElementById("caption");
+//     if (n > slides.length) {slideIndex = 1}
+//     if (n < 1) {slideIndex = slides.length}
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     for (i = 0; i < dots.length; i++) {
+//         dots[i].className = dots[i].className.replace(" active", "");
+//     }
+//     slides[slideIndex-1].style.display = "block";
+//     dots[slideIndex-1].className += " active";
+//     captionText.innerHTML = dots[slideIndex-1].alt;
+//   }
   
 
 
