d8c39911ca0ed166c3bbab8aa1f0fd14a5c0089c
Chris Polishak <chris@spectre.lan1>
2020-01-20 20:11:30 -0600

    changes to index page layout, navbar and icons

--- app.js
+++ app.js
@@ -6,8 +6,32 @@ vanish.classList.add('magictime', 'vanishIn');
 const slideRight = document.querySelector('.slideRight');
 slideRight.classList.add('magictime', 'tinLeftIn');
 
+const slideUpReturn = document.querySelector('.slideUpReturn');
+slideUpReturn.classList.add('magictime', 'slideUpReturn');
+
 const twistIn = document.querySelector('.twistIn');
 twistIn.classList.add('magictime', 'twisterInUp');
+
+
+//Get the button:
+mybutton = document.getElementById(".myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 // function debounce(func, wait = 20, immediate = true) {
 //     var timeout;
