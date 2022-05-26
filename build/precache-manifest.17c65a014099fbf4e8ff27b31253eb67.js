bd9391862aa34208c28156e704402773a2f1dde1
Chris Polishak <chrispolishak@gmail.com>
2022-05-26 12:45:10 -0500

    Trying to add build directory to fix

---
+++ build/precache-manifest.17c65a014099fbf4e8ff27b31253eb67.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "668030695da0e3330a4f63bf65c1d249",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a439174d59b8552b0cb7",
+    "url": "/static/css/main.05b894ec.chunk.css"
+  },
+  {
+    "revision": "b36b3f755a9357463025",
+    "url": "/static/js/2.afaf6e82.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.afaf6e82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a439174d59b8552b0cb7",
+    "url": "/static/js/main.5d7b4e5a.chunk.js"
+  },
+  {
+    "revision": "126c69e9a4b837323d08",
+    "url": "/static/js/runtime-main.a7f3e3d8.js"
+  },
+  {
+    "revision": "d780ce2308ab869a30847f15f9ecce0c",
+    "url": "/static/media/bicycleW.d780ce23.png"
+  },
+  {
+    "revision": "2470c19427f48ebc805a7c5444ab6820",
+    "url": "/static/media/fc02w.2470c194.png"
+  },
+  {
+    "revision": "309da4473921cbb2eb77e200fe705023",
+    "url": "/static/media/zigzag.309da447.png"
+  }
+]);
